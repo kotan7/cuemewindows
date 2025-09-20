33b9bb41fb60203a47d1fe44463dc3252a69b6d7
itsukison <itsukison00@gmail.com>
2025-09-20 16:35:11 +0900

    add

---
+++ package-mac-signed.sh
@@ -0,0 +1,6 @@
+export APPLE_ID="ployee.officialcontact@gmail.com"
+export APPLE_APP_SPECIFIC_PASSWORD="kbfb-tuwf-eagw-bvzq"
+export APPLE_TEAM_ID="4KS6YS23KT"
+export CSC_LINK="./certificate.p12"
+export CSC_KEY_PASSWORD="Rmwtclag$1964"
+npm run package
